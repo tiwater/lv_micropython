bbcae1722001444931504366a557661272e9dd39
Sawyer <sawyer@SawyerdeMacBook-Pro.local>
2021-10-12 23:40:23 +0800

    Support Chinese filename

--- lib/oofatfs/ffconf.h
+++ lib/oofatfs/ffconf.h
@@ -164,7 +164,11 @@
 /  ff_memfree() in ffsystem.c, need to be added to the project. */
 
 
+#ifdef MICROPY_FATFS_LFN_UNICODE
+#define FF_LFN_UNICODE  (MICROPY_FATFS_LFN_UNICODE)
+#else
 #define FF_LFN_UNICODE  0
+#endif
 /* This option switches the character encoding on the API when LFN is enabled.
 /
 /   0: ANSI/OEM in current CP (TCHAR = char)
